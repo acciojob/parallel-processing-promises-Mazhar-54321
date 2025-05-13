7e95f93b2eab30818d7d8ba39cad2a65467c2f52
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-13 16:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+document.addEventListener("DOMContentLoaded",async function(){
+//     <div id="output"></div>
+//   <div id="error"></div>
+//   <div id="loading"></div>
+  var loading = document.getElementById("loading");
+  var error = document.getElementById("error");
+  var output = document.getElementById("output");
+  
+  loading.innerText="Loading..."
+    const array = await Promise.all([fetch(images[0]["url"]),fetch(images[1]["url"]),fetch(images[2]["url"])])
+    if(array){
+     loading.innerText="";
+     console.log(array,"arrau")
+     let imagesSrcArray= [array[0]["url"],array[1]["url"],array[2]["url"]];
+     imagesSrcArray.forEach((el)=>{
+         const img = document.createElement("img");
+         img.src=el;
+         img.style.width="200px";
+         img.style.height="200px"
+         output.appendChild(img);
+     })
+    }else{
+        loading.innerText=""
+        error.innerText="Error downloading images"
+    }
+})
